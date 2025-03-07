cb29f0c0d6007afb7e944f3d5836bfb96e0dafb4
andres-martin-c <172848384+andres-martin-C@users.noreply.github.com>
2025-03-06 19:36:34 -0500

    Add: images and buttons with letters

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import './App.css'
+import "./App.css";
+import { letters } from "./helpers/letters";
 
 function App() {
   return (
-  <div className='App'>
-    <h1>Hola Andres</h1>
-  </div>
-  );
+    <div className="App">
+      {/* Imágenes */}
+      <h3>Imagen del juego</h3>
 
- 
+      {/* Palabra oculta */}
+      <h3>_ _ _ _ _ _ _ _ </h3>
+
+      {/* Contador de intentos */}
+      <h3>Intentos: 0</h3>
+
+      {/* Botones de letras */}
+      {letters.map((letter) => (
+        // Necesitamos poner un parámetros key para que reac sepa que letra es.
+        <button key={letter}>{letter}</button>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
