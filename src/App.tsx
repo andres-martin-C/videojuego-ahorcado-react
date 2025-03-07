272d50770d86aeb513296d5446d6fa9bdd523749
andres-martin-c <172848384+andres-martin-C@users.noreply.github.com>
2025-03-06 20:40:06 -0500

    Create: funcion for contador de attempts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
+import { useState } from "react";
 import "./App.css";
 import { HangImage } from "./components/HangImage";
 import { letters } from "./helpers/letters";
 
 function App() {
+  // Estamos creando una variable de estado
+  const [ attempts, setAttempts] = useState(0);
+
+  // Creacion de una funcion con flecha
+  const checkLetter = (letter: string) => {
+    console.log(letter);
+    setAttempts( Math.min(attempts + 1, 9));
+  }
+
+
   return (
     <div className="App">
       {/* Imágenes */}
-      <HangImage imageNumber = { 10 }/>
+      <HangImage imageNumber = { attempts }/>
 
       {/* Palabra oculta */}
       <h3>_ _ _ _ _ _ _ _ </h3>
 
       {/* Contador de intentos */}
-      <h3>Intentos: 0</h3>
+      <h3>Intentos: {attempts}</h3>
 
       {/* Botones de letras */}
       {letters.map((letter) => (
         // Necesitamos poner un parámetros key para que reac sepa que letra es.
-        <button key={letter}>{letter}</button>
+        <button 
+        onClick={() => checkLetter(letter)}
+        key={letter}>{letter}
+        </button>
       ))}
     </div>
   );
