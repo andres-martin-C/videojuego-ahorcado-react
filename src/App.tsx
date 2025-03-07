252a92430b26b96681021b4369d04ff334989967
andres-martin-c <172848384+andres-martin-C@users.noreply.github.com>
2025-03-06 22:49:39 -0500

    Funcion para ver las letras acertadas

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,41 @@ import { HangImage } from "./components/HangImage";
 import { letters } from "./helpers/letters";
 
 function App() {
-// Palabra
-  const [word] = useState('COMPUTADORA');
+  // Palabra
+  const [word] = useState("COMPUTADORA");
   // useState nos sirve para decir que hay un cambio.
-  const [hiddenWord] = useState('_ '.repeat(word.length));
+  const [hiddenWord, setHiddenWord] = useState("_ ".repeat(word.length));
 
   // Estamos creando una variable de estado
-  const [ attempts, setAttempts] = useState(0);
+  const [attempts, setAttempts] = useState(0);
 
   // Creacion de una funcion con flecha
   const checkLetter = (letter: string) => {
-    console.log(letter);
-    setAttempts( Math.min(attempts + 1, 9));
-  }
+    if (!word.includes(letter)) {
+      setAttempts(Math.min(attempts + 1, 9));
+      return;
+    }
+    
+    // retorna un arreglo y quita todos los espacios.
+    const hiddenWordArray = hiddenWord.split(' ');
 
+    for (let i = 0; i < word.length; i++) {
+      // Si la letra es igual a la que me esta dando entonces lo
+      // remplaza.
+      if (word[i] === letter) {
+        // Remplaza el valor de esa posición por la letra.
+        hiddenWordArray[i] = letter;
+      }
+    }
+
+    setHiddenWord(hiddenWordArray.join(' '));
+    
+  };
 
   return (
     <div className="App">
       {/* Imágenes */}
-      <HangImage imageNumber = { attempts }/>
+      <HangImage imageNumber={attempts} />
 
       {/* Palabra oculta */}
       <h3>{hiddenWord}</h3>
@@ -33,9 +49,8 @@ function App() {
       {/* Botones de letras */}
       {letters.map((letter) => (
         // Necesitamos poner un parámetros key para que reac sepa que letra es.
-        <button 
-        onClick={() => checkLetter(letter)}
-        key={letter}>{letter}
+        <button onClick={() => checkLetter(letter)} key={letter}>
+          {letter}
         </button>
       ))}
     </div>
