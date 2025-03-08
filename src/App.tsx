8b962f3b3e8f3324a360935b240fa5ab124a65ea
andres-martin-c <172848384+andres-martin-C@users.noreply.github.com>
2025-03-07 19:11:30 -0500

    Logica y visualizacion de ganaste

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { HangImage } from "./components/HangImage";
 import { letters } from "./helpers/letters";
@@ -8,12 +8,31 @@ function App() {
   const [word] = useState("COMPUTADORA");
   // useState nos sirve para decir que hay un cambio.
   const [hiddenWord, setHiddenWord] = useState("_ ".repeat(word.length));
-
+  const [ lose, setLose] = useState(false);
+  const [ won, setWon] = useState(false);
   // Estamos creando una variable de estado
   const [attempts, setAttempts] = useState(0);
 
+  // Es una acción que se va a ejecutar cuando nosotros digamos.
+  useEffect( () => {
+    if (attempts >= 9) {
+      setLose(true);
+    }
+    // Se activara cuando hayan cambios de esta variable attempts
+  }, [attempts]); // hooks
+
+  // Es una acción que se va a ejecutar cuando nosotros digamos.
+  useEffect(() => {
+    const currentHiddenWord = hiddenWord.split(' ').join('');
+    if (word === currentHiddenWord) {
+      setWon( true );
+    }
+  }, [hiddenWord]);
+
   // Creacion de una funcion con flecha
   const checkLetter = (letter: string) => {
+    if (lose) return;
+
     if (!word.includes(letter)) {
       setAttempts(Math.min(attempts + 1, 9));
       return;
@@ -45,6 +64,16 @@ function App() {
 
       {/* Contador de intentos */}
       <h3>Intentos: {attempts}</h3>
+      
+      {/* Mensaje de si perdio */}
+      {
+        (lose) ? <h2>Perdio {word}</h2> : ''
+      }
+
+      {/* Mensaje de si gano */}
+      {
+        (won) ? <h2>Felicidades tu ganaste</h2> : ''
+      }
 
       {/* Botones de letras */}
       {letters.map((letter) => (
